87e5ee1eee1904cd994e1ce7ae1fe499d654187a
Benjamin Pannell <admin@sierrasoftworks.com>
2015-10-12 17:40:38 +0200

    feat: Added initial set of commands for CLI

---
+++ commands.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"github.com/EMSSConsulting/Depro/command"
+	"github.com/mitchellh/cli"
+)
+
+// CLI commands to be made available to users
+var Commands map[string]cli.CommandFactory
+
+func init() {
+	ui := &cli.BasicUi{
+		Writer: os.Stdout,
+	}
+
+	Commands = map[string]cli.CommandFactory{
+		"version": func() (cli.Command, error) {
+			ver := Version
+			rel := VersionPrerelease
+
+			if GitDescribe != "" {
+				ver = GitDescribe
+			}
+
+			if GitDescribe == "" && rel == "" {
+				rel = "dev"
+			}
+
+			return &command.VersionCommand{
+				Revision:          GitCommit,
+				Version:           ver,
+				VersionPrerelease: rel,
+				UI:                ui,
+			}, nil
+		},
+
+		"deploy": func() (cli.Command, error) {
+			return &command.DeployCommand{
+				UI: ui,
+			}, nil
+		},
+	}
+}
